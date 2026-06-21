49d69514ce96f8c3d091d2dc970486dbb84e2fa9
digistex4u <info@digistex4u.com>
2026-06-21 20:52:09 +0530

    Webhook: new 9453391715 (Unknown)

--- public/contacts.js
+++ public/contacts.js
@@ -1,4 +1,4 @@
 // Pramogh CRM Contacts Database
-// Webhook update: 2026-06-21T15:19:04.754Z
-// 3 unique contacts
-window.PRAMOGH_CONTACTS = [{"phone":"8847866827","name":"Unknown","updated_at":"2026-06-21T14:50:00.333Z"},{"phone":"7666468331","name":"Unknown","updated_at":"2026-06-21T15:14:44.199Z"},{"phone":"9604170669","name":"Unknown","updated_at":"2026-06-21T15:19:04.569Z"}];
+// Webhook update: 2026-06-21T15:22:09.159Z
+// 4 unique contacts
+window.PRAMOGH_CONTACTS = [{"phone":"8847866827","name":"Unknown","updated_at":"2026-06-21T14:50:00.333Z"},{"phone":"7666468331","name":"Unknown","updated_at":"2026-06-21T15:14:44.199Z"},{"phone":"9604170669","name":"Unknown","updated_at":"2026-06-21T15:19:04.569Z"},{"phone":"9453391715","name":"Unknown","updated_at":"2026-06-21T15:22:08.965Z"}];
