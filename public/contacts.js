13f8a490c114c196473045d66dd34e9af1840db9
digistex4u <info@digistex4u.com>
2026-06-21 20:34:07 +0530

    Webhook: new 7666468331 (Unknown)

--- public/contacts.js
+++ public/contacts.js
@@ -1,4 +1,4 @@
 // Pramogh CRM Contacts Database
-// Webhook update: 2026-06-21T14:50:00.499Z
-// 1 unique contacts
-window.PRAMOGH_CONTACTS = [{"phone":"8847866827","name":"Unknown","updated_at":"2026-06-21T14:50:00.333Z"}];
+// Webhook update: 2026-06-21T15:04:07.527Z
+// 2 unique contacts
+window.PRAMOGH_CONTACTS = [{"phone":"8847866827","name":"Unknown","updated_at":"2026-06-21T14:50:00.333Z"},{"phone":"7666468331","name":"Unknown","updated_at":"2026-06-21T15:04:07.399Z"}];
