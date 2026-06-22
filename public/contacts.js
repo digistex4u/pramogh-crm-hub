44796836bfb46c2811a6cc0b9db1ccc0fb658c3c
digistex4u <info@digistex4u.com>
2026-06-22 10:02:10 +0530

    Webhook: updated 9816284432 (Unknown)

--- public/contacts.js
+++ public/contacts.js
@@ -1,4 +1,4 @@
 // Pramogh CRM Contacts Database
-// Webhook update: 2026-06-22T04:32:09.536Z
+// Webhook update: 2026-06-22T04:32:10.793Z
 // 2 unique contacts
-window.PRAMOGH_CONTACTS = [{"phone":"9724451087","name":"Unknown","updated_at":"2026-06-22T04:31:05.081Z"},{"phone":"9816284432","name":"Unknown","updated_at":"2026-06-22T04:32:07.581Z"}];
+window.PRAMOGH_CONTACTS = [{"phone":"9724451087","name":"Unknown","updated_at":"2026-06-22T04:31:05.081Z"},{"phone":"9816284432","name":"Unknown","updated_at":"2026-06-22T04:32:10.713Z"}];
