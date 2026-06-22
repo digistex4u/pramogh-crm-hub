509db78b84c0650cafd3e6000754485341600e4c
digistex4u <info@digistex4u.com>
2026-06-22 10:04:28 +0530

    Webhook: updated 9999986661 (Unknown)

--- public/contacts.js
+++ public/contacts.js
@@ -1,4 +1,4 @@
 // Pramogh CRM Contacts Database
-// Webhook update: 2026-06-22T04:34:26.623Z
+// Webhook update: 2026-06-22T04:34:28.528Z
 // 5 unique contacts
-window.PRAMOGH_CONTACTS = [{"phone":"9724451087","name":"Unknown","updated_at":"2026-06-22T04:31:05.081Z"},{"phone":"9816284432","name":"Unknown","updated_at":"2026-06-22T04:33:30.907Z"},{"phone":"9604170669","name":"Unknown","updated_at":"2026-06-22T04:33:24.451Z"},{"phone":"9131594933","name":"Unknown","updated_at":"2026-06-22T04:33:52.758Z"},{"phone":"9999986661","name":"Unknown","updated_at":"2026-06-22T04:34:26.518Z"}];
+window.PRAMOGH_CONTACTS = [{"phone":"9724451087","name":"Unknown","updated_at":"2026-06-22T04:31:05.081Z"},{"phone":"9816284432","name":"Unknown","updated_at":"2026-06-22T04:33:30.907Z"},{"phone":"9604170669","name":"Unknown","updated_at":"2026-06-22T04:33:24.451Z"},{"phone":"9131594933","name":"Unknown","updated_at":"2026-06-22T04:33:52.758Z"},{"phone":"9999986661","name":"Unknown","updated_at":"2026-06-22T04:34:27.185Z"}];
