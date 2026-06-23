d58cf10232a9c3a2b2d08feab3a07bb8e191bfa8
digistex4u <info@digistex4u.com>
2026-06-24 04:28:58 +0530

    Add files via upload

---
+++ public/automation-log.js
@@ -0,0 +1,4 @@
+// Pramogh CRM Automation Execution Log
+// Updated: 2026-06-24T00:00:00.000Z
+// 0 entries
+window.PRAMOGH_AUTOMATION_LOG = [];
---
+++ public/automations-data.js
@@ -0,0 +1,4 @@
+// Pramogh CRM Automation Rules
+// Updated: 2026-06-24T00:00:00.000Z
+// 0 automations
+window.PRAMOGH_AUTOMATIONS = [];
